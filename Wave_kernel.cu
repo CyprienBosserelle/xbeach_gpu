3461158dbe4fbde04b053e8112873b7135fd889a
CyprienBosserelle <cyprien.bosserelle@gmail.com>
2016-10-04 12:49:50 +1200

    Fixed issue on compiling

--- Wave_kernel.cu
+++ Wave_kernel.cu
@@ -152,7 +152,7 @@ __global__ void WAVEDT(int nx, int ny, DECNUM dtheta, DECNUM *dtwave, DECNUM *ct
 	unsigned int i = ix + iy*nx;
 	if (ix < nx && iy < ny)
 	{
-		dtflow[i] = dtheta / (ctheta[i]);
+		dtwave[i] = dtheta / (ctheta[i]);
 	}
 }
 
