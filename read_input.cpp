fee24b448974f2ea2740b841acf36f673b4d51e6
Cyprien <cyprien.bosserelle@gmail.com>
2021-08-27 17:04:00 +1200

    Fix_ input issues

--- read_input.cpp
+++ read_input.cpp
@@ -1515,7 +1515,7 @@ XBGPUParam checkparamsanity(XBGPUParam XParam, std::vector<SLBnd> slbnd, std::ve
 	if (XParam.outvars.empty() && XParam.outputtimestep > 0)
 	{
 		//a nc file was specified but no output variable were specified
-		std::vector<std::string> SupportedVarNames = { "zb", "zs", "uu", "vv", "H", "thetamean", "D", "urms", "ueu", "vev", "C", "dzb", "Fx", "Fy", "hh", "Hmean", "uumean", "vvmean", "hhmean", "zsmean", "Cmean" };
+		std::vector<std::string> SupportedVarNames = { "zb", "zs", "uu", "vv", "H", "thetamean", "D", "urms", "ueu", "vev", "C", "dzb", "Fx", "Fy", "hh", "Hmean", "uumean", "vvmean", "hhmean", "zsmean", "Cmean", "ueumean", "vevmean" };
 		for (int isup = 0; isup < SupportedVarNames.size(); isup++)
 		{
 			XParam.outvars.push_back(SupportedVarNames[isup]);
